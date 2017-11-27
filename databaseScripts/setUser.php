2517b8ab60737e23bff47b672f8c95c7645b801c
Sultan Alshamsi <7620888@gmail.com>
2017-11-27 17:09:09 -0500

    created login handeling

--- databaseScripts/setUser.php
+++ databaseScripts/setUser.php
@@ -18,9 +18,24 @@ $sql = "INSERT INTO user (name, password)
 VALUES ('$name', '$password2')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo '<script language="javascript">';
+    echo 'alert("User Created Successfully")';
+    echo '</script>';
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $sql = "Select name, password from user where name='$name' and password='$password2'";
+    $result = $conn->query($sql);
+    if(!$result || $result->num_rows <= 0)
+    {
+      echo '<script language="javascript">';
+      echo 'alert("The username or password does not match")';
+      echo '</script>';
+      $_SESSION['logged_in'] = 1;
+      }else {
+        echo '<script language="javascript">';
+        echo 'alert("User Logged in Successfully")';
+        echo '</script>';
+      }
+    //echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
